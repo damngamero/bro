a0e3cc20d47bc8eb6fb1ab45296ab04d32473e9f
Haisom khan <devil.gamer.maestro@gmail.com>
2025-09-05 19:51:26 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+  },
 };
 
 export default nextConfig;
